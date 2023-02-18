d0f7e9ef2602def10993e27777a7cbe1f1ac0525
DevenMahesh <125744262+DevenMahesh@users.noreply.github.com>
2023-02-18 22:44:34 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -3,8 +3,8 @@
 using namespace std;
 
 int main(void) {
-  cout << "Hello, World\n";
-  cout << "Hello, Jenkins\n";
-  cout << "I have successfully built and run\n";
+  cout << "Hey, World\n";
+  cout << "Hey, Jenkins\n";
+  cout << "Hey I'm Deven Mahesh\n";
   return 0;
 }
